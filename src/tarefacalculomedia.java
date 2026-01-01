57be64fbb28d5d3a852188411008461806f051c0
Daniel Fernandes <daniel.fernandes.tr@gmail.com>
2026-01-01 18:06:13 -0300

    firts commit

---
+++ src/tarefacalculomedia.java
@@ -0,0 +1,15 @@
+import org.w3c.dom.Notation;
+
+public class tarefacalculomedia {
+    public static void main(String[] args) {
+        int nota = 5;
+        int nota1 = 3;
+        int nota2 = 7;
+
+        int media = (nota + nota1 + nota2) /3;
+        System.out.println("Primeira nota " + nota);
+        System.out.println("Segunda nota " + nota1);
+        System.out.println("Terceira nota " + nota2);
+        System.out.println("Media: "+ media);
+    }
+}
