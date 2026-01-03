6865b2b7916e2754c067274a2fb2048444d2a698
Daniel Fernandes <daniel.fernandes.tr@gmail.com>
2026-01-03 17:11:42 -0300

    Adicinando a quarta nota

--- src/tarefacalculomedia.java
+++ src/tarefacalculomedia.java
@@ -5,11 +5,13 @@ public class tarefacalculomedia {
         int nota = 5;
         int nota1 = 3;
         int nota2 = 7;
+        int nota3= 8;
 
-        int media = (nota + nota1 + nota2) /3;
+        int media = (nota + nota1 + nota2 + nota3) /3;
         System.out.println("Primeira nota " + nota);
         System.out.println("Segunda nota " + nota1);
         System.out.println("Terceira nota " + nota2);
+        System.out.println("Quarta nota " + nota3);
         System.out.println("Media: "+ media);
     }
 }
